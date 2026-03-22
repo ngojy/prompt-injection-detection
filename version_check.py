1e41c83b41ced3866185ea8676eb250d71eac903
Jeremy N <112124475+ngojy@users.noreply.github.com>
2026-03-22 14:29:53 -0400

    added requirements.txt and version_check.py, and updated main.ipynb

---
+++ version_check.py
@@ -0,0 +1,13 @@
+import numpy
+import pandas
+import sklearn
+import notebook
+import datasets
+import sentence_transformers
+
+print("NumPy:", numpy.__version__)
+print("Pandas:", pandas.__version__)
+print("Scikit-learn:", sklearn.__version__)
+print("Jupyter Notebook:", notebook.__version__)
+print("Datasets:", datasets.__version__)
+print("Sentence-Transformers:", sentence_transformers.__version__)
